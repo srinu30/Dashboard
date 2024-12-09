94a38036e24f410d971e7172bf56526c61113abe
srinu30 <batchusrinivas30@gmail.com>
2024-12-09 06:42:44 +0000

    Solution

--- src/components/UnitToggle/index.js
+++ src/components/UnitToggle/index.js
@@ -5,7 +5,6 @@ const UnitToggle = ({unit, onToggle}) => (
     <button onClick={onToggle}>
       Switch to {unit === 'C' ? 'Fahrenheit' : 'Celsius'}
     </button>
-    const await = (unit,onToggle), const data = JSON.stringfy(data),
   </div>
 )
 
